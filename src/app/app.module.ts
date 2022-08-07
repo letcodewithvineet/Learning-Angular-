33b93aaad9bf86bcb860f71851effa680ec53697
Vineet Pandey <finalsolutioncode@gmail.com>
2022-08-07 20:18:01 +0530

    'component2'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -7,13 +11,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
+    MatListModule,
     BrowserModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
